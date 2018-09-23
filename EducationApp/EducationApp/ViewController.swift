3e0b32bd588e24f8f72ed27e27a76056719e545e
Thomas Lisankie <thomas.lisankie@gmail.com>
2018-09-23 17:42:58 -0400

    Initial commit
    Contains the initial app project generated by Xcode and CocoaPods.

---
+++ EducationApp/EducationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EducationApp
+//
+//  Created by Thomas Lisankie on 9/23/18.
+//  Copyright © 2018 Thomas Lisankie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
