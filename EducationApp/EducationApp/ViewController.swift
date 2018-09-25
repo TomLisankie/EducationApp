d8027ae488efde3252478688c9b774a25eba7297
Thomas Lisankie <thomas.lisankie@gmail.com>
2018-09-25 14:12:32 -0400

    Began work on interface.

--- EducationApp/EducationApp/ViewController.swift
+++ EducationApp/EducationApp/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func logInChoice(_ sender:UIButton){
+        
+        sender.setTitle("Hello", for: .normal)
+        
+    }
 
 }
 
